0003f2bea26a56a9d85e60c7f190013c78e21c56
hallgato <hallgato@LBOR210-08>
2023-03-21 10:43:07 +0100

    Add project files.

---
+++ CudaRuntime1/kernel.cu
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+#include<device_launch_parameters.h>
+#define BLOCK_SIZE 16
+
+__global__ void matrixMul(float* A, float* B, float* C, int n) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < n && col < n) {
+        float sum = 0.0f;
+        for (int i = 0; i < n; i++) {
+            sum += A[row * n + i] * B[i * n + col];
+        }
+        C[row * n + col] = sum;
+    }
+}
+
+int main() {
+    int n = 1024;
+    size_t size = n * n * sizeof(float);
+
+    float* h_A = (float*)malloc(size);
+    float* h_B = (float*)malloc(size);
+    float* h_C = (float*)malloc(size);
+
+    for (int i = 0; i < n * n; i++) {
+        h_A[i] = rand() / (float)RAND_MAX;
+        h_B[i] = rand() / (float)RAND_MAX;
+    }
+
+    float* d_A, * d_B, * d_C;
+    cudaMalloc((void**)&d_A, size);
+    cudaMalloc((void**)&d_B, size);
+    cudaMalloc((void**)&d_C, size);
+
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 dimGrid((n + dimBlock.x - 1) / dimBlock.x, (n + dimBlock.y - 1) / dimBlock.y);
+
+    matrixMul << <dimGrid, dimBlock >> > (d_A, d_B, d_C, n);
+
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+    for (int i = 0; i < n * n; i++) {
+        if (h_C[i] != h_A[i] * h_B[i]) {
+            printf("Error: Matrix multiplication is incorrect!\n");
+            break;
+        }
+    }
+
+    free(h_A);
+    free(h_B);
+    free(h_C);
+
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    return 0;
+}
